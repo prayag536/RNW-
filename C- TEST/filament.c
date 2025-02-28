1ac45bde5055aa1ad812924364b40aaa660a5737
prayag536 <prayagsabhaya77@gmail.com>
2025-02-28 20:22:50 +0530

    filament test.c

---
+++ C- TEST/filament.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    int length,Palindrome = 1;
+
+    printf("Enter any string:");
+    scanf("%s", str);
+
+    for (int i = 0; i < 1/ 2; i++)
+    {
+        if (str[i] != str[length - i - 1])
+        
+        {
+            Palindrome = 0;
+        }
+    }
+
+    if (Palindrome)
+    {
+        printf("The given string is a palindrome\n");
+    }
+    
+    else
+    {
+        printf("Not a palindrome\n");
+    }
+
+    return 0;
+}
+
+--------------------------------------------------------------------
+
+#include <stdio.h>
+int main() {
+    char str[500], ch;
+    int count = 0;
+
+    printf("Enter a string:");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Enter a character to find its frequency:");
+    scanf("%c", &ch);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (ch == str[i]) ++count;
+    }
+
+    printf("Frequency of %c = %d", ch, count);
+    return 0;
+}
+
